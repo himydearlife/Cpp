3621779df372375b8705491bfb4355f9a09c58a1
himydearlife <64508553+himydearlife@users.noreply.github.com>
2022-04-28 19:56:24 +0800

    Update 210.cpp

--- leetcode/210.cpp
+++ leetcode/210.cpp
@@ -28,3 +28,30 @@ public:
         return res.size() == numCourses ? res : vector<int> {};
     }
 };
+// 2 拓扑排序不需要再次记录边
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> in_cnt(numCourses, 0);
+        for (auto& p : prerequisites) {
+            ++in_cnt[p[0]];
+        }
+        queue<int> que;
+        for (int i = 0; i < numCourses; ++i) {
+            if (in_cnt[i] == 0) que.push(i);
+        }
+        vector<int> res;
+        while (!que.empty()) {
+            int curr = que.front();
+            que.pop();
+            res.push_back(curr);
+            for (auto& p : prerequisites) {
+                if (p[1] == curr) {
+                    --in_cnt[p[0]];
+                    if (in_cnt[p[0]] == 0) que.push(p[0]); 
+                }
+            }
+        }
+        return res.size() == numCourses ? res : vector<int> {};
+    }
+};
